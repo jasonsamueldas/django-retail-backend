d444497f1ebcff3170be2dd471c64f61715e738c
Jason Samuel Das <138836129+jasonsamueldas@users.noreply.github.com>
2026-04-06 12:18:13 +0530

    Update settings.py

--- retail_project/settings.py
+++ retail_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uo$7&cl00v4tu7^84p6oli5bzp4+iq4^29w5gfd5ruiu3jhima'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
